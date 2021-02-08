587e769fcfb30c41e3caf7d6c15e13ebbc7301a1
kawasaky007 <vuhoanganptit@gmail.com>
2021-02-08 13:52:48 +0700

    footer

--- Kito_app/src/app/main/tonggiaophan/tonggiaophan.page.ts
+++ Kito_app/src/app/main/tonggiaophan/tonggiaophan.page.ts
@@ -23,29 +23,24 @@ title = '(Tổng) Giáo phận'
   }
 
   ngOnInit() {
-    if (this.platform.is("android")) {
-      document.querySelector<HTMLElement> ('.content-search').style.marginBottom ="10px";
-      document.querySelector<HTMLElement> ('.list-item').style.marginTop ="20px";
-      document.querySelector<HTMLElement> ('.icon-bars').style.transform ="translateY(25%)"
-
-    }
+    
      
   }
   
-  async openModalMenu() {
-    const popover = await this.modalCtrl.create({
-      component: ModalMenuComponent,
-      cssClass: 'modalMenu  ',
-    });
-    return await popover.present();
-  }
-  toggleHideSearchBar(value) {
-    event.stopPropagation();
-    this.hiddenSearchBar = value;
-    if (!value) {
-      this.searchBar.setFocus();
-    }
-  }
+  // async openModalMenu() {
+  //   const popover = await this.modalCtrl.create({
+  //     component: ModalMenuComponent,
+  //     cssClass: 'modalMenu  ',
+  //   });
+  //   return await popover.present();
+  // }
+  // toggleHideSearchBar(value) {
+  //   event.stopPropagation();
+  //   this.hiddenSearchBar = value;
+  //   if (!value) {
+  //     this.searchBar.setFocus();
+  //   }
+  // }
   checkoutVatican(){
     this.router.navigateByUrl('main/tonggiaophan/parish-news');
     
