450f0a24c64bf89e470398eb654bd1678d0621bd
Phước Tài <73770102+phuoctai12t@users.noreply.github.com>
2021-02-19 14:33:59 +0700

    Fix main (#51)
    * fix empty events text at prayer time
    
    * fix main ui
    
    * fix get user info logic

--- Kito_app/src/app/main/main.page.ts
+++ Kito_app/src/app/main/main.page.ts
@@ -12,29 +12,100 @@ export class MainPage implements OnInit {
   name = '';
   avatar = 'assets/img/avatar-account.svg';
 
+  menu = [
+    {
+      name: '(Tổng) Giáo phận',
+      thumbImage: 'assets/img/menu/tonggiaophan.svg',
+      desUrl: 'main/tonggiaophan',
+      fullWidth: true
+    },
+    {
+      name: 'Tin tức giáo xứ',
+      thumbImage: 'assets/img/menu/tintuc.svg',
+      desUrl: 'main/tonggiaophan',
+    },
+    {
+      name: 'Chi tiết giờ lễ',
+      thumbImage: 'assets/img/menu/chitietgiole.svg',
+      desUrl: 'main/prayer-time',
+    },
+    {
+      name: 'Lớp học Giáo lý',
+      thumbImage: 'assets/img/menu/lophocgiaoly.svg',
+      desUrl: 'main/catechism-class',
+    },
+    {
+      name: 'Đóng góp',
+      thumbImage: 'assets/img/menu/donggop.svg',
+      desUrl: 'donate',
+    },
+    {
+      name: 'Xin lễ',
+      thumbImage: 'assets/img/menu/xinle.svg',
+      desUrl: 'pray',
+    },
+    {
+      name: 'Cửa hàng',
+      thumbImage: 'assets/img/menu/cuahang.svg',
+      desUrl: 'main/store',
+    },
+  ]
+
+  news = [
+    {
+      title: 'ĐTC Phanxicô cử hành Thánh lễ Ngày Đời sống Thánh hiến',
+      thumbImage: 'assets/img/bgnew.jpg'
+    },
+    {
+      title: 'ĐTC Phanxicô cử hành Thánh lễ Ngày Đời sống Thánh hiến',
+      thumbImage: 'assets/img/bgnew.jpg'
+    },
+    {
+      title: 'ĐTC Phanxicô cử hành Thánh lễ Ngày Đời sống Thánh hiến',
+      thumbImage: 'assets/img/bgnew.jpg'
+    },
+    {
+      title: 'ĐTC Phanxicô cử hành Thánh lễ Ngày Đời sống Thánh hiến',
+      thumbImage: 'assets/img/bgnew.jpg'
+    },
+  ]
+
   constructor(
     private router: Router,
     private OneSignalService: OneSignalService,
     private accountService: AccountService
   ) { }
-  ionViewWillEnter () {
+
+  ionViewWillEnter() {
     this.accountService.getAccounts().subscribe((data) => {
-      localStorage.setItem('avatar', data.app_user.thumb_image.url);
-      localStorage.setItem('fullname', data.app_user.full_name)
+      this.avatar = data.app_user.thumb_image.url || '';
+      this.name = data.app_user.full_name || '';
+      localStorage.setItem('avatar', this.avatar);
+      localStorage.setItem('fullname', this.name)
     })
-    if(localStorage.getItem('avatar')) {
-      this.avatar = localStorage.getItem('avatar');
-    }
-    this.name = localStorage.getItem('fullname');
   }
+
   ngOnInit() {
     this.OneSignalService.startOneSignal();
   }
-  routerLink(path) {
-    this.router.navigateByUrl(path);
-  } 
 
-  // goToUserInfo() {
-  //   this.router.navigateByUrl('account-setting');
-  // }
+  goToDetail(item) {
+    this.router.navigateByUrl(item.desUrl);
+  }
+
+  goToNewsDetail(item) {
+    const data = {
+      id: item.id,
+      type: 'News'
+    }
+    this.router.navigate(['/news-detail'], {
+      queryParams: {
+        data: JSON.stringify(data)
+      }
+    })
+  }
+
+  goToAccountSetting() {
+    this.router.navigateByUrl('account-setting');
+  }
 }
