befbba4e53858610a8961222517bbeeb881a2034
Phước Tài <73770102+phuoctai12t@users.noreply.github.com>
2021-02-24 11:00:01 +0700

    fix-custom-header (#75)

--- Kito_app/src/app/@modular/header/header.component.ts
+++ Kito_app/src/app/@modular/header/header.component.ts
@@ -8,7 +8,7 @@ import { Router } from '@angular/router';
   styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent implements OnInit {
-  @Input() headerCustom: {background?: String, title: String};
+  @Input() headerCustom: {title?: String, background?: String};
   
   constructor( public router: Router 
    
