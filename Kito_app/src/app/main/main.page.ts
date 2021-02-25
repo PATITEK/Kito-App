8a724a609120bdfc546ac19e65b4f36b763ca227
Bao Dang <danglequocbao001@gmail.com>
2021-02-25 10:01:23 +0700

    fix-load-avatar

--- Kito_app/src/app/main/main.page.ts
+++ Kito_app/src/app/main/main.page.ts
@@ -84,39 +84,42 @@ export class MainPage implements OnInit {
     this.name = localStorage.getItem('fullname');
     // this.imageService.getImage();
     this.accountService.getAccounts().subscribe(data => {
-      if(data.app_user.thumb_image == null) {
+      if (data.app_user.thumb_image == null) {
         data.app_user['thumb_image'] = "https://i.imgur.com/edwXSJa.png";
         this.avatar = data.app_user.thumb_image;
+        localStorage.setItem('avatar', this.avatar);
       }
-      else if( data.app_user.thumb_image.url == null) {
+      else if (data.app_user.thumb_image.url == null) {
         data.app_user['thumb_image'] = "https://i.imgur.com/edwXSJa.png";
         this.avatar = data.app_user.thumb_image;
+        localStorage.setItem('avatar', this.avatar);
       }
       else {
-        this.avatar =  data.app_user.thumb_image.url;
+        this.avatar = data.app_user.thumb_image.url;
+        localStorage.setItem('avatar', this.avatar);
       }
-  })
-}
+    })
+  }
 
   ngOnInit() {
     this.OneSignalService.startOneSignal();
     this.name = localStorage.getItem('fullname');
-   
+
   }
   goToDetail(item) {
-    if(item.desUrl == 'donate') {
+    if (item.desUrl == 'donate') {
       const data = {
         type: 'donate'
       }
       this.authService.sendData(data)
     }
-    else if(item.desUrl == 'pray') {
+    else if (item.desUrl == 'pray') {
       const data = {
         type: 'pray'
       }
       this.authService.sendData(data)
     }
-     this.router.navigateByUrl(item.desUrl);
+    this.router.navigateByUrl(item.desUrl);
   }
 
   goToNewsDetail(item) {
