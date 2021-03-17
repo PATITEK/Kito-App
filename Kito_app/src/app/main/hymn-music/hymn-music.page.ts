37ea0d1b3caee2c017e62d28012af3925587b4f0
Phước Tài <73770102+phuoctai12t@users.noreply.github.com>
2021-03-17 11:38:53 +0700

    Fix music (#135)
    * add duration music

--- Kito_app/src/app/main/hymn-music/hymn-music.page.ts
+++ Kito_app/src/app/main/hymn-music/hymn-music.page.ts
@@ -254,4 +254,16 @@ export class HymnMusicPage implements OnInit {
     }
     return tempArr;
   }
+
+  secondsToHms(d) {
+    d = Number(d);
+    var h = Math.floor(d / 3600);
+    var m = Math.floor(d % 3600 / 60);
+    var s = Math.floor(d % 3600 % 60);
+
+    var hDisplay = h > 0 ? h + ':' : "";
+    var mDisplay = m > 0 ? m + ':' : "0:";
+    var sDisplay = s > 0 ? s + '' : "0";
+    return hDisplay + mDisplay + sDisplay;
+  }
 }
