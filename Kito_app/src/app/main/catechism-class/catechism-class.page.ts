f98b05e1c990092cc8f670816e5da92432541f7e
Dang Le Quoc Bao <danglequocbao001@gmail.com>
2021-03-23 16:11:10 +0700

    Update catechism-class.page.ts

--- Kito_app/src/app/main/catechism-class/catechism-class.page.ts
+++ Kito_app/src/app/main/catechism-class/catechism-class.page.ts
@@ -7,7 +7,7 @@ import { Router } from '@angular/router';
   styleUrls: ['./catechism-class.page.scss'],
 })
 export class CatechismClassPage implements OnInit {
-  headerCustom = {title: 'Lớp học giáo lý'};
+  headerCustom: any;
 
   catechismList = [
     {
