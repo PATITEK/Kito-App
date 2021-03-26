aea14611cc592de1e018316f5f396203736cf71f
phuoctai12t <phuoctai12t@gmail.com>
2021-03-26 21:00:26 +0700

    fix conflict

--- Kito_app/src/app/@app-core/http/calendar/calendar.DTO.ts
+++ Kito_app/src/app/@app-core/http/calendar/calendar.DTO.ts
@@ -1,7 +1,7 @@
 import { IPageRequest } from "../global";
 
 export interface IPageCalendar extends IPageRequest {
-    cal_date?: string;
+    cal_date?: string | Date;
 }
 export interface Day {
     valueView: string;
